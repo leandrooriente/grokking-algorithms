88e1e971000be4b35a8ef893b906ec1f5efe9333
leandrooriente <leandro.oriente@dazn.com>
2021-06-25 13:56:41 +0200

    Add binary search implementation

--- exercises/binary-search.test.ts
+++ exercises/binary-search.test.ts
@@ -4,4 +4,11 @@ describe("Binary Search", () => {
   it("should return null if not found", () => {
     expect(binarySearch([], 2)).toBeNull();
   });
+
+  it("should return the index if found", () => {
+    expect(binarySearch([0, 1, 2], 2)).toBe(2);
+    expect(binarySearch([0, 1, 2], 0)).toBe(0);
+    expect(binarySearch([2], 2)).toBe(0);
+    expect(binarySearch([0, 23, 35, 79, 143, 244], 143)).toBe(4);
+  });
 });
--- exercises/binary-search.ts
+++ exercises/binary-search.ts
@@ -1,3 +1,19 @@
 export const binarySearch = (list: number[], item: number): number | null => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    let middle = Math.floor(high - low / 2);
+    if (list[middle] === item) {
+      return middle;
+    }
+
+    if (list[middle] > item) {
+      high = middle - 1;
+    } else {
+      low = middle + 1;
+    }
+  }
+
   return null;
 };
